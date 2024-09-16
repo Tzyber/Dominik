6d56a5d6cc662cfd343ce4f625552471a6abb39d
tzyber <dominiksasu@hotmail.de>
2024-09-16 12:16:26 +0200

    updates packages minor changes

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,10 +7,10 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AppFooter: typeof import('./components/AppFooter.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     Navigation: typeof import('./components/Navigation.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    ThreeScene: typeof import('./components/ThreeScene.vue')['default']
   }
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -54,7 +54,7 @@
       <div>
       <v-row class="mb-4" style="display: flex; justify-content: center;">
         <v-col cols="12" md="3" sm="4" >
-          <v-card class="project-card pa-4">
+          <v-card class="project-card pa-4" style="min-height: 600px">
             <v-img
               :src="projectImage1"
               alt="Project Image 1"
@@ -63,7 +63,7 @@
             <h3>Twitch Chat Bot</h3>
             <br>
             <p> A Twitch chat bot that alerts the chat whenever someone subscribes, gifts subs, or cheers with Bits. It can also be used as a quiz bot.</p>
-            <div class="mt-2"> <p><a class="links" href="https://github.com/Tzyber/twitchChatBot" target="_blank">Check Code Here</a></p></div>
+            <div class="mt-2"><p><a class="links" href="https://github.com/Tzyber/twitchChatBot" target="_blank">Check Code Here</a></p></div>
             <div class="mt-2">
               <v-chip class="mr-2">Javascript</v-chip>
               <v-chip class="mr-2">Express</v-chip>
@@ -71,7 +71,7 @@
           </v-card>
         </v-col>
         <v-col cols="12" md="3" sm="4" >
-          <v-card class="project-card pa-4">
+          <v-card class="project-card pa-4" style="min-height: 600px">
             <v-img
               :src="projectImage2"
               alt="Portfolio"
@@ -80,7 +80,7 @@
             <h3>Portfolio</h3>
             <br>
             <p>My new  Portfolio written in Vue, Vuetify and typescript. Currently this Active page. Still in Development but further updates are comming like new Projects or other  ideas</p>
-
+            <div class="mt-2"><p><a class="links" href="https://github.com/Tzyber/Dominik" target="_blank">Check Code Here</a></p></div>
             <div class="mt-4">
               <v-chip class="mr-2">typescript</v-chip>
               <v-chip class="mr-2">Vuetify</v-chip>
@@ -89,7 +89,7 @@
           </v-card>
         </v-col>
         <v-col cols="12" md="3" sm="4">
-          <v-card class="project-card pa-4">
+          <v-card class="project-card pa-4" style="min-height: 600px">
             <v-img
               :src="projectImage3"
               alt="Project Image 3"
@@ -98,13 +98,13 @@
             <h3>Netflix Recreation</h3>
             <br>
             <p>My small Recreation of Netflix. there might still be some Bugs but those will be resolved in the future. Github Pages is a bit picky with images.</p>
-            <div class="mt-2"> <p><a class="links" href="https://tzyber.github.io/netflix-clone/" target="_blank">Check Demo Here</a></p></div>
+            <div class="mt-2"> <p><a class="links" href="https://tzyber.github.io/netflix-clone/" target="_blank">Check Demo Here</a></p> or<p><a class="links" href="https://github.com/Tzyber/netflix-clone" target="_blank">Check Code Here</a></p></div>
             <div class="mt-2">
             <v-chip class="mr-2">typescript</v-chip>
             <v-chip class="mr-2">tailwind</v-chip>
             <v-chip>Vue</v-chip>
           </div>
-   
+  
           </v-card>
         </v-col>
       </v-row>
@@ -135,7 +135,6 @@
               </div>
             </v-card-text>
           </v-card>
-              
         </v-col>
       </v-row>
     </section>
