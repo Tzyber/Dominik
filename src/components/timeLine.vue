14389b26212abf2642beb5eead96913e4b25f611
Tzyber <134324777+Tzyber@users.noreply.github.com>
2025-09-17 18:24:46 +0200

    Update timeLine.vue
    updated timeline

--- src/components/timeLine.vue
+++ src/components/timeLine.vue
@@ -4,7 +4,6 @@
       <v-timeline
         v-if="isLargeScreen"
         direction="horizontal"
-        align="start"
         class="mt-8"
       >
         <v-timeline-item :dot-color="'#ff0096b3'">
@@ -12,81 +11,99 @@
             <div>
               <div class="text-h6">Selbstständig</div>
               <p>Selbstständig als Webdesigner und Entwickler</p>
-            </div>
-          </template>
+          </div>
+        </template>
           x - 2018
         </v-timeline-item>
-  
         <v-timeline-item>
-          <template v-slot:opposite>2018-2019</template>
-          <div>
+          <template v-slot:opposite>
             <div class="text-h6">Mammut Media</div>
             <p>Webdesign und Entwicklung von Komponenten und Websites speziell für die Kunden.</p>
-          </div>
+        </template>
+          2018-2019
         </v-timeline-item>
-  
         <v-timeline-item :dot-color="'#ff0096b3'">
           <template v-slot:opposite>
             <div>
               <div class="text-h6">Docunite Gmbh</div>
               <p>Solution Developer (Webentwickler) für Docunite.</p>
-            </div>
-          </template>
+        </div>
+        </template>
           2020-2024
         </v-timeline-item>
+        <v-timeline-item>
+          <template v-slot:opposite>
+            <div>
+            <div class="text-h6">Umschulung</div>
+              <p>Umschulung zum Fachinformatiker in Anwendungsentwicklung</p>
+          </div>
+          </template>
+          2025
+        </v-timeline-item>
       </v-timeline>
-  
+
       <!-- Vertical Timeline (für kleine Bildschirme) -->
       <v-timeline
         v-else
         direction="vertical"
-        align="start"
         class="mt-8"
       >
-      <v-timeline-item :dot-color="'#ff0096b3'">
+        <v-timeline-item>
           <template v-slot:opposite>
             <div>
               <div class="text-h6">Selbstständig</div>
               <p>Selbstständig als Webdesigner und Entwickler</p>
-            </div>
-          </template>
+          </div>
+        </template>
           x - 2018
         </v-timeline-item>
-  
         <v-timeline-item>
-          <template v-slot:opposite>2018-2019</template>
-          <div>
-            <div class="text-h6">Mammut Media</div>
-            <p>Webdesign und Entwicklung von Komponenten und Websites speziell für die Kunden.</p>
-          </div>
+          <template v-slot:opposite>
+            <div>
+              <div class="text-h6">Mammut Media</div>
+              <p>Webdesign und Entwicklung von Komponenten und Websites speziell für die Kunden.</p>
+            </div>
+          </template>
+          2018-2019
         </v-timeline-item>
-  
+
         <v-timeline-item :dot-color="'#ff0096b3'">
-          <template v-slot:opposite >
+          <template v-slot:opposite>
             <div>
               <div class="text-h6">Docunite Gmbh</div>
               <p>Solution Developer (Webentwickler) für Docunite.</p>
-            </div>
-          </template>
+        </div>
+        </template>
           2020-2024
+        </v-timeline-item>
+        <v-timeline-item>
+          <template v-slot:opposite>
+            <div>
+            <div class="text-h6">Umschulung</div>
+              <p>Umschulung zum Fachinformatiker in Anwendungsentwicklung</p>
+          </div>
+          </template>
+          2025
         </v-timeline-item>
       </v-timeline>
     </div>
   </template>
-  
+
   <script lang="ts">
   import { defineComponent, computed } from 'vue';
-  import { useDisplay } from 'vuetify';
-  
-  export default defineComponent({
-    setup() {
-      const { mdAndUp } = useDisplay();
-      const isLargeScreen = computed(() => mdAndUp.value);
-  
-      return {
-        isLargeScreen,
-      };
-    },
-  });
-  </script>
-  
+import { useDisplay } from 'vuetify';
+
+export default defineComponent({
+  setup() {
+    const { mdAndUp } = useDisplay();
+    const isLargeScreen = computed(() => mdAndUp.value);
+
+    return {
+      isLargeScreen,
+    };
+  },
+});
+    </script>
+<style scoped>
+
+</style>
